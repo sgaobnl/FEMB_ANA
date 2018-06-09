1b615e2942198d98eff09e04d332525a6a1f9ee1
Shanshan Gao <gao33.bnl@gmail.com>
2018-06-09 12:13:33 -0400

    update

--- M8_run.sh
+++ M8_run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ####         argv[0]                  argv[1]        argv[2] argv[3] argv[4]  argv[5] argv[6] argv[7]  argv[8] 
 ####                                  mon_date_year, run_no, APAno,    jumbo   gain,    tp,    server,  plot_en
-###python  M8_ProtoDUNE_coldbox.py     "06_08_2018"    "01"      9      False    1       3     "mac"    "0x08" &  
-python  M8_ProtoDUNE_coldbox.py     "06_08_2018"    "11"      9      False    1       3     "mac"     "0x0F"   
+python  M8_ProtoDUNE_coldbox.py       "06_08_2018"    "11"      9      False    1       3     "mac"     "0x0F"   
