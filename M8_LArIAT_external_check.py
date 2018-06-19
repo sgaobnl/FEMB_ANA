6981eb049c080ad236beec57fa6dfad33b6e1331
Shanshan Gao <gao33.bnl@gmail.com>
2018-06-19 17:09:35 -0500

    update

--- M8_LArIAT_external_check.py
+++ M8_LArIAT_external_check.py
@@ -5,7 +5,7 @@ Author: GSS
 Mail: gao.hillhill@gmail.com
 Description: 
 Created Time: 7/15/2016 11:47:39 AM
-Last modified: Tue Jun 19 13:58:06 2018
+Last modified: Tue Jun 19 17:09:11 2018
 """
 import matplotlib
 matplotlib.use('Agg')
@@ -372,7 +372,7 @@ def plots(plt, plot_en, apa_results, loginfo, run_temp, sort_np_flg, pp, gain=2,
                     y_np = np.array(chn_wave[chn])
                     y_max = np.max(y_np)
                     #y_np = y_np - ped_np[chn]
-                    y_np = y_np - ped_np[chn]
+                    y_np = y_np 
                     smps_np = np.arange(len(chn_wave[chn])) 
                     x_np = smps_np * 0.5
                     ax.scatter( x_np, y_np)
@@ -870,6 +870,7 @@ result_waveform = result_dir + "X" + format(plot_en, "02X") + rundir +  "_" + ap
 pp = PdfPages(result_pdf)
 
 mode = "E" 
+gain = "X"
 wib_np = [0,1]
 feed_freq=500
 wibsdata = All_FEMBs_results(path, rundir, apamap.APA, APAno, gain=gain, mode=mode, wib_np = wib_np, tp=tp, jumbo_flag = jumbo_flag, feed_freq = 500, hp_filter=hp_filter)
--- all_FEMBs_results.py
+++ all_FEMBs_results.py
@@ -5,7 +5,7 @@ Author: GSS
 Mail: gao.hillhill@gmail.com
 Description: 
 Created Time: 7/15/2016 11:47:39 AM
-Last modified: Tue Jun 19 10:25:59 2018
+Last modified: Tue Jun 19 16:52:51 2018
 """
 
 #defaut setting for scientific caculation
@@ -73,10 +73,10 @@ def All_FEMBs_results(path, rundir,  APA="ProtoDUNE", APAno =1,  gain=3, mode=0,
                     femb = int( rawfilep[rawfilep.find("FEMB") + 4])
                     chip = int( rawfilep[rawfilep.find("CHIP") + 4])
                     filetp = int( rawfilep[rawfilep.find("CHIP") + 6],16) & 0x03
-
+                    CFG_flg = (rawfilep.find("CFG_DATA")  > 0 )
                     apamap.femb = wib*4+femb
                     apa_femb_loc, X_sort, V_sort, U_sort = apamap.apa_femb_mapping()
-                    if os.path.isfile(rawfilep) and (filetp==tp) :
+                    if os.path.isfile(rawfilep) and ( (filetp==tp) or CFG_flg) :
                         with open(rawfilep, 'rb') as f:
                             raw_data = f.read()                
                             len_file = len(raw_data) 
